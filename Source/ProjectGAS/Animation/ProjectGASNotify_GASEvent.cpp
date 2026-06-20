b6b12a2dc51302b21497807b8b38bcd56ae1e568
DaaE <say_that_u@naver.com>
2026-06-20 23:59:33 +0900

    wip: 콤보 윈도우/카운트 Effect, AnimNotify 이벤트 추가 (콤보 재진입 버그 - 재설계 필요)

---
+++ Source/ProjectGAS/Animation/ProjectGASNotify_GASEvent.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectGASNotify_GASEvent.h"
+#include "AbilitySystemBlueprintLibrary.h"
+// SendGameplayEventToActor가 정의된 헤더
+
+void UProjectGASNotify_GASEvent::Notify(
+	USkeletalMeshComponent* MeshComp,
+	UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (AActor* Owner = MeshComp->GetOwner())
+	{
+		FGameplayEventData EventData;
+		// Payload는 지금 비워둠 - 나중에 추가 데이터 필요하면 여기 채움
+
+		UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, EventTag, EventData);
+		// Owner가 IAbilitySystemInterface를 구현하지 않으면 조용히 실패함
+		// 우리 AProjectGASCharacter는 이미 구현했으니 안전
+	}
+}
---
+++ Source/ProjectGAS/Animation/ProjectGASNotify_GASEvent.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "ProjectGASNotify_GASEvent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGAS_API UProjectGASNotify_GASEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	// 에디터에서 어떤 이벤트인지 태그로 지정
+	// SaveAttack Notify에는 Combo.Window.Open
+	// ResetCombo Notify에는 별도 종료용 태그를 지정할 거예요
+	UPROPERTY(EditAnywhere, Category = "GAS")
+	FGameplayTag EventTag;
+
+	virtual void Notify(USkeletalMeshComponent* MeshComp,
+		UAnimSequenceBase* Animation,
+		const FAnimNotifyEventReference& EventReference) override;
+};
